a91f238bcd84ee921ed3ebda8d8abdf830f859dc
shawn-phy <philipoyuga14@gmail.com>
2024-06-16 00:40:19 +0300

    feat: last working version users

--- event_management_module/lib/users_page.dart
+++ event_management_module/lib/users_page.dart
@@ -28,11 +28,15 @@ class UsersPage extends StatelessWidget {
           if (result.data == null) {
             return const Center(child: Text('No data found.'));
           }
-          print(result.data);
-          
-          List users = result.data?['getAttendees'];
 
-          if (users == null) {
+          
+          print(result.data); // debug statement 
+
+          List<dynamic> users = result.data?['attendees'] ?? []; 
+          //ffs I am tired of runtime errors
+
+          // checking if users list is empty. 
+          if (users.isEmpty) {
             print('No users found');
             return const Center(child: Text('No users found.'));
           }
@@ -40,7 +44,7 @@ class UsersPage extends StatelessWidget {
           return ListView.builder(
             itemCount: users.length,
             itemBuilder: (context, index) {
-              final user = users[index];
+              final user = users[index] as Map<String, dynamic>;
               return ListTile(
                 title: Text('${user['firstName']} ${user['lastName']}'),
                 subtitle: Text(user['email']),
