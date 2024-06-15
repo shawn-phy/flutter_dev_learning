316d1553ba03290abb259edf8abd9b257f57e64c
shawn-phy <philipoyuga14@gmail.com>
2024-06-15 22:49:47 +0300

    fix: debugging https client

--- event_management_module/lib/graphql_client.dart
+++ event_management_module/lib/graphql_client.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class GraphQLConfig {
-  static final HttpLink httpLink = HttpLink('https://localhost:7046/graphql/');
+  static final HttpLink httpLink = HttpLink(
+    'https://10.0.2.2:7046/graphql', // For Android emulator
+    // 'https://localhost:5001/graphql', // For iOS simulator
+    // 'https://<host-machine-ip>:5001/graphql', // For physical device
+    defaultHeaders: {
+      'Content-Type': 'application/json',
+    },
+  );
 
   static ValueNotifier<GraphQLClient> initializeClient() {
     ValueNotifier<GraphQLClient> client = ValueNotifier(
